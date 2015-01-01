433a70f2fff4e18ae88e1f8f392d5a9fa4c807ee
Mike Vella <vellamike@gmail.com>
2015-01-01 20:49:52 +0000

    added another test method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn add_one(x: int) -> int {
+    x + 1i
+}
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests{
+    #[test]
+    fn it_works() {
+    }
+
+    #[test]
+    fn test_adds_one() {
+        assert_eq!(3i, super::add_one(2i));
+    }
+
 }
